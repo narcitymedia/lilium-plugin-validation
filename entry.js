e8d2c0e51093189004876d5c425a3fede8d7d816
Erik Desjardins <erik.desjardins@gmail.com>
2019-09-12 18:12:40 +0000

    Validation plugin will ignore business site

--- entry.js
+++ entry.js
@@ -9,15 +9,21 @@ class ArticleValidation {
 
     register(_c, info, done) {
         hooks.bind('article_validating', 1, extra => {
+            if (extra._c && extra._c.uid == "3873934738971003280") {
+                return;
+            }
+
             if (!extra.post.isSponsored) {
                 if (extra.post.title[0].length > 90) {
                     extra.errors.title = { message: 'The article title must be shorter than 90 characters. The title has ' + extra.post.title[0].length + ' characters.', setBy: 'article_validation' };
+                } else if (extra.post.seotitle.length > 90) {
+                    extra.errors.seotitle = { message: 'The article SEO title must be shorter than 90 characters. The SEO title has ' + extra.post.title[0].length + ' characters.', setBy: 'article_validation' };
                 }
 
                 const contentDOM = new JSDOM(extra.post.content[0]);
                 const doc =contentDOM.window.document;
                 const ads = doc.querySelectorAll('.lml-adplaceholder');
-                if (!ads.length) extra.errors.content = { message: 'The content must include at least 1 advertisement', setBy: 'article_validation' };
+                if (!ads.length) extra.errors.content = { message: 'The content must include at least 1 ad banner', setBy: 'article_validation' };
             }
         });
 
